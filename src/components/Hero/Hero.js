411fbfeb50f4512971dd1f974c9b71cc15162344
haroonbajwa <haroonbajwa99@gmail.com>
2021-10-17 22:25:10 +0500

    hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,12 +2,22 @@ import React from 'react';
 
 import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import { LeftSection, MyName } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hey there! <br />
+        I'm Haroon Bajwa <br />
+        Web Developer
+      </SectionTitle>
+      <SectionText>
+        I am a React JS and WordPress web developer and have done many projects. I have extensive knowledge of HTML, CSS, Javascript and many other web languages.
+      </SectionText>
+      <Button onClick={() => window.location="https://google.com"}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
